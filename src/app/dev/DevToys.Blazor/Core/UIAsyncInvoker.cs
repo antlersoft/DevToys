45cfdd6429fe80a618ad2dcc1b677f92d23264c3
antlersoft <ads@antlersoft.com>
2025-12-16 20:51:42 +0000

    Get component handle from main Razor component

--- src/app/dev/DevToys.Blazor/Core/UIAsyncInvoker.cs
+++ src/app/dev/DevToys.Blazor/Core/UIAsyncInvoker.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Components;
 
 namespace DevToys.Blazor.Core;
 
-public sealed class UIAsyncInvoker : ComponentBase, Api.IUIAsyncInvoker
+public sealed class UIAsyncInvoker : Api.IUIAsyncInvoker
 {
+    private ComponentBase? _component;
+
+    public void SetComponent(ComponentBase component)
+    {
+        _component = component;
+    }
+
     public void AsyncInvoker(EventHandler handler)
     {
-        InvokeAsync(() => handler(null, EventArgs.Empty));
+        _component?.InvokeAsync(() => handler(null, EventArgs.Empty));
     }
 }
