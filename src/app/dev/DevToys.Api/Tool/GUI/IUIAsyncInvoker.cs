0d628fd4bbb60d31c5c9d566ca4ae21964767802
antlersoft <ads@antlersoft.com>
2025-12-16 17:53:33 +0000

    Move GetUIAsyncInvoker into DevToys.Api

--- src/app/dev/DevToys.Api/Tool/GUI/IUIAsyncInvoker.cs
+++ src/app/dev/DevToys.Api/Tool/GUI/IUIAsyncInvoker.cs
@@ -4,3 +4,13 @@ public interface IUIAsyncInvoker
 {
     void AsyncInvoker(EventHandler handler);
 }
+
+public static partial class GUI
+{
+    internal static IUIAsyncInvoker? UIAsyncInvokerInstance { get; set; }
+
+    public static IUIAsyncInvoker GetUIAsyncInvoker()
+    {
+        return UIAsyncInvokerInstance!;
+    }
+}
